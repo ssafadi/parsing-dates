c63c87bb26994bf317cd31e427ee380da0edda86
ssafadi <samisaf@outlook.com>
2021-04-10 11:26:56 -0400

    1 file modified

--- Main.tsx
+++ Main.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 
 export default function Main(props){
-    const [dateStr, setDateStr] = React.useState('05-15-2019');
-
+    const [dateStr, setDateStr] = React.useState('2019-05-01');
     const parsed = new Date(dateStr);
-    console.log(parsed)
+    const today = new Date();
   return(
 
     <div>
     <label>Enter date to parse</label>
-    <input onChange={(e => setDateStr(e.target.value))} />
+    <input placeholder={dateStr} onChange={(e => setDateStr(e.target.value))} />
     <div>Provided String: {dateStr}</div>
     <div>Parsed Date {parsed.toString()}</div>
+
+    <div>Today's Date {today.toString()}</div>
+    <div>Provided Date Has Past: {parsed < today? 'Yes': 'No'}</div>
+
+
     </div>
   );
 }
