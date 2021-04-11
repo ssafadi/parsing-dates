57f21aebddb20e8c314331b9662585ef99ccda19
ssafadi <samisaf@outlook.com>
2021-04-10 21:50:43 -0400

    1 file modified

--- Main.tsx
+++ Main.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
+function add2today({ days = 0, months = 0, years = 0 }) {
+    const today = new Date();
+    const yyyy = today.getFullYear() + years;
+    const mm = today.getMonth() + months;
+    const dd = today.getDay() + days;
+    return `${yyyy}-${mm}-${dd}`
+}
+
 export default function Main(props){
     const [dateStr, setDateStr] = React.useState('2019-05-01');
     const parsed = new Date(dateStr);
@@ -14,7 +22,7 @@ export default function Main(props){
 
     <div>Today's Date: {today.toString()}</div>
     <div>Provided Date Has Passed: {parsed < today? 'Yes': 'No'}</div>
-
+    <div>a year from today is {add2today({year : 1})}</div>
 
     </div>
   );
