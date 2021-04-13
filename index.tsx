dea15ce2d75f818c1b01fa71dfb7831120ecb44c
ssafadi <samisaf@outlook.com>
2021-04-12 22:54:29 -0400

    1 file modified

--- index.tsx
+++ index.tsx
@@ -4,6 +4,14 @@ import Main from './Main';
 // import 'milligram/dist/milligram.min.css';
 import './style.css';
 
+const epochSecs = 1618270892 * 1000;
+const date = new Date(epochSecs);
+console.log(date)
+const year = date.getUTCFullYear();
+const month = date.getUTCMonth()+1;
+const day = date.getUTCDate();
+console.log(year, month, day);
+
 function App(props){
   return(
   <div id='app'>
